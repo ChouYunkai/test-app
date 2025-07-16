dcb7d2748f5764ef633181d6c6f82fcffbc196f3
ChouYunkai <1207099632@qq.com>
2025-07-16 10:34:12 +0800

    增加了i18n

---
+++ src/public/i18n/en.js
@@ -0,0 +1,19 @@
+export default {
+    messages: {
+        home: 'Home',
+        about: 'About',
+        contact: 'Contact',
+        add: 'Add',
+        edit: 'Edit',
+        delete: 'Delete',
+        save: 'Save',
+        cancel: 'Cancel',
+    },
+    person: {
+        name: 'Name',
+        age: 'Age',
+        email: 'Email',
+        phone:'phone',
+        identity:'identity',
+    }
+};
