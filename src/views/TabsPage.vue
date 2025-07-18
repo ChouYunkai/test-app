491e708a024f008b62cab787be23850d02e24064
ChouYunkai <1207099632@qq.com>
2025-07-18 09:30:15 +0800

    修改了tab1

--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -4,13 +4,13 @@
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon aria-hidden="true" :icon="triangle" />
+          <ion-icon :icon="home" size="large"></ion-icon>
           <ion-label>Home</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon aria-hidden="false" :icon="ellipse" />
-          <ion-label>Tab 2</ion-label>
+          <ion-icon :icon="person" size="large"></ion-icon>
+          <ion-label>Account</ion-label>
         </ion-tab-button>
 
         <!-- <ion-tab-button tab="tab3" href="/tabs/tab3">
@@ -24,5 +24,5 @@
 
 <script setup lang="ts">
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { ellipse,  triangle } from 'ionicons/icons';
+import { home, person } from 'ionicons/icons';
 </script>
